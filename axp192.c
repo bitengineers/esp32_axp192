453f59e326e12d5680ef358d0ffac2459759cee5
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-09-25 18:36:53 +0900

    Functions related to ADC and TS pin

--- axp192.c
+++ axp192.c
@@ -14,8 +14,12 @@
 #define AXP192_SDA      21
 #define AXP192_SCL      22
 
-#define AXP192_REG_POWER_OUTPUT   0x12
-#define AXP192_REG_CHG_CONTROL    0x33
+#define AXP192_REG_POWER_OUTPUT     0x12
+#define AXP192_REG_CHG_CONTROL1     0x33
+#define AXP192_REG_CHG_CONTROL2     0x34
+#define AXP192_REG_ADC_ENABLE1      0x82
+#define AXP192_REG_ADC_ENABLE2      0x83
+#define AXP192_REG_ADC_SAMPLERATE   0x84
 
 static uint8_t status[0];
 static void axp192_i2c_write(uint8_t reg, uint8_t value);
@@ -190,7 +194,7 @@ void axp192_ldo3(bool on)
 
 void axp192_chgen(bool on)
 {
-  _axp192_reg_toggle(AXP192_REG_CHG_CONTROL, on, 0x80);
+  _axp192_reg_toggle(AXP192_REG_CHG_CONTROL1, on, 0x80);
 }
 
 void axp192_chg_set_target_vol(axp192_chg_vol_t vol)
@@ -211,7 +215,7 @@ void axp192_chg_set_target_vol(axp192_chg_vol_t vol)
     value = 0x40;
     break;
   }
-  _axp192_reg_update(AXP192_REG_CHG_CONTROL, value, 0x60);
+  _axp192_reg_update(AXP192_REG_CHG_CONTROL1, value, 0x60);
 }
 
 void axp192_chg_set_end_current(axp192_chg_end_cur_t cur)
@@ -226,7 +230,7 @@ void axp192_chg_set_end_current(axp192_chg_end_cur_t cur)
   default:
     break;
   }
-  _axp192_reg_update(AXP192_REG_CHG_CONTROL, value, 0x10);
+  _axp192_reg_update(AXP192_REG_CHG_CONTROL1, value, 0x10);
 }
 
 void axp192_chg_set_current(axp192_chg_cur_t cur)
@@ -258,16 +262,25 @@ void axp192_chg_set_current(axp192_chg_cur_t cur)
     value = 0x07;
     break;
   }
-  _axp192_reg_update(AXP192_REG_CHG_CONTROL, value, 0x07);
+  _axp192_reg_update(AXP192_REG_CHG_CONTROL1, value, 0x07);
+}
+
+void axp192_chg_ext_pass_en(bool on)
+{
+  uint8_t value = 0x00;
+  if (on) {
+    value = 0x02;
+  }
+  _axp192_reg_update(AXP192_REG_CHG_CONTROL2, value, 0x02);
 }
 
 // 0x78 0x79 Battery voltage
-uint16_t axp192_batt_get_vol(void)
+uint16_t axp192_batt_vol_get(void)
 {
   uint8_t v;
   uint16_t rtn = 0x0000;
   axp192_i2c_read(0x78, &v);
-  rtn = v << 4;
+  rtn = (v << 4);
   v = 0x00;
   axp192_i2c_read(0x79, &v);
   rtn |= v;
@@ -275,7 +288,7 @@ uint16_t axp192_batt_get_vol(void)
 }
 
 // 0x7a 0x7b Battery charge current
-uint16_t axp192_batt_get_chrg_current(void)
+uint16_t axp192_batt_chrg_cur_get(void)
 {
   uint8_t v;
   uint16_t rtn = 0x0000;
@@ -288,7 +301,7 @@ uint16_t axp192_batt_get_chrg_current(void)
 }
 
 // 0x7c 0x7d Battery discharge current
-uint16_t axp192_batt_get_dischrg_current(void)
+uint16_t axp192_batt_dischrg_cur_get(void)
 {
   uint8_t v;
   uint16_t rtn = 0x0000;
@@ -298,4 +311,214 @@ uint16_t axp192_batt_get_dischrg_current(void)
   axp192_i2c_read(0x7d, &v);
   rtn |= v;
   return rtn;
+}
+
+// 0x82 ADC Enable 1
+void axp192_adc_batt_vol_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x80;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x80);
+}
+
+void axp192_adc_batt_cur_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x40;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x40);
+}
+
+void axp192_adc_ac_vol_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x20;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x20);
+}
+
+void axp192_adc_ac_cur_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x10;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x10);
+}
+
+void axp192_adc_vbus_vol_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x08;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x08);
+}
+
+void axp192_adc_vbus_cur_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x04;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x04);
+  axp192_i2c_read(AXP192_REG_ADC_ENABLE1, &v);
+  ESP_LOGI(TAG, "AXP192_REG_ADC_ENABLE1 = %#x", v);
+}
+
+void axp192_adc_aps_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x01;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x02);
+}
+
+void axp192_adc_ts_pin_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x01;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE1, v, 0x01);
+}
+
+// 0x83 ADC Enable 2
+
+void axp192_adc_temp_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x80;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE2, v, 0x80);
+}
+
+void axp192_adc_gpio0_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x08;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE2, v, 0x08);
+}
+
+void axp192_adc_gpio1_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x04;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE2, v, 0x04);
+}
+
+void axp192_adc_gpio2_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x02;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE2, v, 0x02);
+}
+
+void axp192_adc_gpio3_en(bool on)
+{
+  uint8_t v = 0x00;
+  if (on) {
+    v = 0x01;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_ENABLE2, v, 0x01);
+}
+
+// 0x84 ADC samplerate, TS pin contro
+
+void axp192_adc_samplerate_set(axp192_adc_samplerate_t rate)
+{
+  uint8_t v = 0x00;
+  switch(rate) {
+  case AXP192_ADC_SAMPLERATE_25:
+    v = 0x00;
+    break;
+  case AXP192_ADC_SAMPLERATE_50:
+    v = 0x40;
+    break;
+  case AXP192_ADC_SAMPLERATE_100:
+    v = 0x80;
+    break;
+  case AXP192_ADC_SAMPLERATE_200:
+    v = 0xC0;
+    break;
+  default:
+    ESP_LOGE(TAG, "invalid parameter rate = %d", rate);
+    return;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_SAMPLERATE, v, 0xC0);
+}
+
+void axp192_ts_cur_set(axp192_ts_out_cur_t output)
+{
+  uint8_t v = 0x00;
+  switch(output) {
+  case AXP192_TS_OUT_CUR_20U:
+    v = 0x00;
+    break;
+  case AXP192_TS_OUT_CUR_40U:
+    v = 0x10;
+    break;
+  case AXP192_TS_OUT_CUR_60U:
+    v = 0x20;
+    break;
+  case AXP192_TS_OUT_CUR_80U:
+    v = 0x30;
+    break;
+  default:
+    ESP_LOGE(TAG, "invalid parameter output");
+    return;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_SAMPLERATE, v, 0x30);
+}
+
+void axp192_ts_function_set(axp192_ts_func_t func)
+{
+  uint8_t v = 0x00;
+  switch(func) {
+  case AXP192_TS_FUNCTION_BATT_TEMP:
+    v = 0x00;
+    break;
+  case AXP192_TS_FUNCTION_EXT_IN:
+    v = 0x04;
+    break;
+  default:
+    ESP_LOGE(TAG, "invalid parameter func");
+    return;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_SAMPLERATE, v, 0x04);
+}
+
+void axp192_ts_output_en(axp192_ts_out_mode_t mode)
+{
+  uint8_t v = 0x00;
+  switch(mode) {
+  case AXP192_TS_OUT_MODE_OFF:
+    v = 0x00;
+    break;
+  case AXP192_TS_OUT_MODE_CHARGING:
+    v = 0x01;
+    break;
+  case AXP192_TS_OUT_MODE_ADCSAMPLING:
+    v = 0x02;
+    break;
+  case AXP192_TS_OUT_MODE_ON:
+    v = 0x03;
+    break;
+  default:
+    ESP_LOGE(TAG, "invalid parameter mode");
+    return;
+  }
+  _axp192_reg_update(AXP192_REG_ADC_SAMPLERATE, v, 0x03);
 }
--- include/axp192.h
+++ include/axp192.h
@@ -37,6 +37,32 @@ extern "C" {
     AXP192_CHG_CUR_700,
   } axp192_chg_cur_t;
 
+  typedef enum {
+    AXP192_ADC_SAMPLERATE_25,
+    AXP192_ADC_SAMPLERATE_50,
+    AXP192_ADC_SAMPLERATE_100,
+    AXP192_ADC_SAMPLERATE_200,
+  } axp192_adc_samplerate_t;
+
+  typedef enum {
+    AXP192_TS_OUT_CUR_20U,
+    AXP192_TS_OUT_CUR_40U,
+    AXP192_TS_OUT_CUR_60U,
+    AXP192_TS_OUT_CUR_80U,
+  } axp192_ts_out_cur_t;
+
+  typedef enum {
+    AXP192_TS_FUNCTION_BATT_TEMP,
+    AXP192_TS_FUNCTION_EXT_IN,
+  } axp192_ts_func_t;
+
+  typedef enum {
+    AXP192_TS_OUT_MODE_OFF,
+    AXP192_TS_OUT_MODE_CHARGING,
+    AXP192_TS_OUT_MODE_ADCSAMPLING,
+    AXP192_TS_OUT_MODE_ON,
+  } axp192_ts_out_mode_t;
+
   void axp192_init(void);
   void axp192_deinit(void);
 
@@ -68,6 +94,29 @@ extern "C" {
   uint16_t axp192_batt_get_chrg_current(void);
   // 0x7c 7d Battery discharge current
   uint16_t axp192_batt_get_dischrg_current(void);
+  // 0x82 ADC Enable 1
+  void axp192_adc_batt_vol_en(bool on);
+  void axp192_adc_batt_cur_en(bool on);
+  void axp192_adc_ac_vol_en(bool on);
+  void axp192_adc_ac_cur_en(bool on);
+  void axp192_adc_vbus_vol_en(bool on);
+  void axp192_adc_vbus_cur_en(bool on);
+  void axp192_adc_aps_en(bool on);
+  void axp192_adc_ts_pin_en(bool on);
+
+  // 0x83 ADC Enable 2
+  void axp192_adc_temp_en(bool on);
+  void axp192_adc_gpio0_en(bool on);
+  void axp192_adc_gpio1_en(bool on);
+  void axp192_adc_gpio2_en(bool on);
+  void axp192_adc_gpio3_en(bool on);
+
+  // 0x84 ADC samplerate, TS pin control
+  void axp192_adc_samplerate_set(axp192_adc_samplerate_t rate);
+  void axp192_ts_cur_set(axp192_ts_out_cur_t output);
+  void axp192_ts_function_set(axp192_ts_func_t func);
+  void axp192_ts_output_en(axp192_ts_out_mode_t mode);
+
 
 #ifdef __cplusplus
 }
